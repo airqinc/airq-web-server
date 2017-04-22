ce98a7d8bbb65f240927413c4a4f1c08c5de7e6a
albermorap <albermorap@gmail.com>
2017-04-22 20:02:22 +0200

    version inicial

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: 'AirQ',
+  description: 'Web Oficial de AirQ',
+  domain: 'localhost',
+  url: 'http://localhost',
+  env: 'development',
+  port: 3000,
+
+  database: {
+    domain: 'localhost',
+    name: 'nameDb'
+  }
+}
